bc15e8c894746888c583f45ebad0f2545e67e8df
YeshNels <paleojosh@sbcglobal.net>
2022-11-30 14:04:54 -0800

    Adds nested Axios gets to pull two sets of weather data for further use.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var searchbtn = document.getElementById('citysearch')
 var searchval = document.getElementById('cityname')
+var weatherAPI = "b7e8d3a80e5e67f5ecd82ef0bc763cfd"
+
 
 searchbtn.addEventListener("click", function() {
     console.log(searchval.value)
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchval.value + "&appid=" + weatherAPI;
+    axios.get(queryURL)
+    .then((response) => {console.log(response.data);
+    axios.get("https://api.openweathermap.org/data/3.0/onecall?lat="+response.data.coord.lat+"&lon="+response.data.coord.lon+"&exclude=hourly,daily&appid="+weatherAPI)
+    .then(res => {
+        console.log(res.data)
+        document.getElementById('current').innerHTML=`
+        <h2>${response.data.name}`
+    })})
 });
