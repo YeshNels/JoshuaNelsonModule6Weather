78818580891bf349db351359f92cfa17a6afce74
YeshNels <paleojosh@sbcglobal.net>
2022-11-30 11:31:25 -0800

    Adds a search button

---
+++ script.js
@@ -0,0 +1,6 @@
+var searchbtn = document.getElementById('citysearch')
+var searchval = document.getElementById('cityname')
+
+searchbtn.addEventListener("click", function() {
+    console.log(searchval.value)
+});
