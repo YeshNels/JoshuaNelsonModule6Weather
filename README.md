821599252c0ae07713c95a43c357860f635a1e99
YeshNels <paleojosh@sbcglobal.net>
2022-11-30 17:41:40 -0800

    Adds buttons to trigger past searches

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JoshuaNelsonModule6Weather
 A quick weather application.
+
+This fetches data from openweathermap.org to display forecasts for up to five days in the future. To display the UV index, I used two different API calls, one which does the bulk of the forecast and one which require longitude and latitude coordinates and supplies the UVI.
+To make these separate API calls work together, I nested them one within the other and used axios.get rather than fetch to tinker with the two datasets having different variable names.
+To handle search history, the former cities that have been searched are a locally stored array that begins as blank and then has each city that is searched loaded into it. To prevent adding the same city over and over, the button to trigger a past search will run a separate instance of the same search function without the push part added to it. It is still possible to load this array with the same city if the main search bar is used multiple times, but I find this acceptable.
+The code for the forecast is largely repeated with the changes in which part of the dataset they draw from being what creates each instance with its own icon.
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var searchbtn = document.getElementById('citysearch')
 var searchval = document.getElementById('cityname')
 var weatherAPI = "b7e8d3a80e5e67f5ecd82ef0bc763cfd"
+var storedCities = JSON.parse(localStorage.getItem('searchHistory')) || []
 
 
 searchbtn.addEventListener("click", function() {
     console.log(searchval.value)
+    storedCities.push(searchval.value)
+    localStorage.setItem('searchHistory', JSON.stringify(storedCities))
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchval.value + "&units=metric&appid=" + weatherAPI;
     axios.get(queryURL)
     .then((response) => {console.log(response.data);
@@ -57,3 +60,64 @@ searchbtn.addEventListener("click", function() {
         <p>Wind speed is ${response.data.list[39].wind.speed}m/s</p>`
     })})
 });
+
+storedCities.forEach(city => {
+    document.getElementById('history').innerHTML += `
+    <button data-city=${city} class="past">${city}</button>`
+});
+//Currently only adds the buttons on page reload: I accept this.
+var memories = document.getElementsByClassName("past");
+for (let i = 0; i < memories.length; i++) {
+    memories[i].addEventListener("click", function(){
+    var historyURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + event.target.dataset.city + "&units=metric&appid=" + weatherAPI;
+    axios.get(historyURL)
+    .then((response) => {console.log(response.data);
+    axios.get("https://api.openweathermap.org/data/3.0/onecall?lat="+response.data.city.coord.lat+"&lon="+response.data.city.coord.lon+"&exclude=hourly,minutely&units=metric&appid="+weatherAPI)
+    .then(res => {
+        console.log(res.data)
+        if (res.data.current.uvi<2) {
+            uvi="green"
+        }
+        else if (res.data.current.uvi<5) {
+            uvi="orange"
+        }
+        else {
+            uvi="red"
+        }
+        document.getElementById('current').innerHTML=`
+        <h2>Current forecast for ${response.data.city.name}</h2>
+        <p>${response.data.list[0].dt_txt}</p>
+        <img src="http://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png">
+        <p>Temp is ${response.data.list[0].main.temp}C</p>
+        <p>Humidity is ${response.data.list[0].main.humidity}%</p>
+        <p>Wind speed is ${response.data.list[0].wind.speed}m/s</p>
+        <p class="${uvi}">UV index is ${res.data.current.uvi}</p>`
+        document.getElementById('future').innerHTML=`
+        <h2>Five-day forecast for ${response.data.city.name}</h2>
+        <p>${response.data.list[8].dt_txt}</p>
+        <img src="http://openweathermap.org/img/wn/${response.data.list[8].weather[0].icon}@2x.png">
+        <p>Temp is ${response.data.list[8].main.temp}C</p>
+        <p>Humidity is ${response.data.list[8].main.humidity}%</p>
+        <p>Wind speed is ${response.data.list[8].wind.speed}m/s</p>
+        <p>${response.data.list[16].dt_txt}</p>
+        <img src="http://openweathermap.org/img/wn/${response.data.list[16].weather[0].icon}@2x.png">
+        <p>Temp is ${response.data.list[16].main.temp}C</p>
+        <p>Humidity is ${response.data.list[16].main.humidity}%</p>
+        <p>Wind speed is ${response.data.list[16].wind.speed}m/s</p>
+        <p>${response.data.list[24].dt_txt}</p>
+        <img src="http://openweathermap.org/img/wn/${response.data.list[24].weather[0].icon}@2x.png">
+        <p>Temp is ${response.data.list[24].main.temp}C</p>
+        <p>Humidity is ${response.data.list[24].main.humidity}%</p>
+        <p>Wind speed is ${response.data.list[24].wind.speed}m/s</p>
+        <p>${response.data.list[32].dt_txt}</p>
+        <img src="http://openweathermap.org/img/wn/${response.data.list[32].weather[0].icon}@2x.png">
+        <p>Temp is ${response.data.list[32].main.temp}C</p>
+        <p>Humidity is ${response.data.list[32].main.humidity}%</p>
+        <p>Wind speed is ${response.data.list[32].wind.speed}m/s</p>
+        <p>${response.data.list[39].dt_txt}</p>
+        <img src="http://openweathermap.org/img/wn/${response.data.list[39].weather[0].icon}@2x.png">
+        <p>Temp is ${response.data.list[39].main.temp}C</p>
+        <p>Humidity is ${response.data.list[39].main.humidity}%</p>
+        <p>Wind speed is ${response.data.list[39].wind.speed}m/s</p>`
+    })})
+})}
